612ddf9df324b531373807adba9cf5dbf03e733b
Nico Herrera <nherrera0831@gmail,com>
2021-02-03 22:09:14 -0800

    error with cart being undefined

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
 
   const addItem = (item) => {
-    setCart(item);
+    setCart(...cart, { item });
     // add the given item to the cart
   };
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <CartContext.Provider value={cart}>
+        <Navigation />
 
-      {/* Routes */}
-      <ProductContext.Provider value={{ products, addItem }}>
-        <Route exact path="/">
-          <Products />
-        </Route>
+        {/* Routes */}
+        <ProductContext.Provider value={{ products, addItem }}>
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </ProductContext.Provider>
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </ProductContext.Provider>
+      </CartContext.Provider>
     </div>
   );
 }
